d98f9812e93953cb312790a42d7a79644fe45344
o-semenkova <semenkova.oksana.work@gmail.com>
2015-02-19 18:01:13 +0200

    HW 6 - RequireJS
    Underscore added to UsersManagement app

--- osemenkova/UsersManagement/js/Views/AddView.js
+++ osemenkova/UsersManagement/js/Views/AddView.js
@@ -2,11 +2,14 @@
  * Created by osemenkova on 2/16/2015.
  */
 
-define(function(){
+define(["underscore"],function(_){
 
     function render(parameters){
-        var appDiv = document.getElementById('app');
-        appDiv.innerHTML = '<input id="user-name" /><button id="add">Add this user</button>';
+        //var appDiv = document.getElementById('app');
+        //appDiv.innerHTML = '<input id="user-name" /><button id="add">Add this user</button>';
+        var templateContent = $("#addUserTemplate").html();
+        var evaluated = _.template(templateContent)();
+        $("#addUser").html(evaluated);
     }
 
     return {
--- osemenkova/UsersManagement/js/Views/ListView.js
+++ osemenkova/UsersManagement/js/Views/ListView.js
@@ -2,17 +2,23 @@
  * Created by osemenkova on 2/16/2015.
  */
 
-define(function(){
+define(["underscore"],function(_){
     function render(parameters){
-        var appDiv = document.getElementById('app');
+        //var appDiv = document.getElementById('app');
+        //var users = parameters.users;
+        //var html = '<ul>';
+        //
+        //for (var i = 0, len = users.length; i < len; i++){
+        //    html += '<li>' + users[i].name + '</li>';
+        //}
+        //html += '</ul>';
+        //appDiv.innerHTML = html;
         var users = parameters.users;
-        var html = '<ul>';
-
-        for (var i = 0, len = users.length; i < len; i++){
-            html += '<li>' + users[i].name + '</li>';
-        }
-        html += '</ul>';
-        appDiv.innerHTML = html;
+        var templateContent = $("#usersTemplate").html();
+        alert("templateContent:" + templateContent);
+        var evaluated = _.template(templateContent)({users: users});
+        alert("evaluated: " + evaluated);
+        $("#listUsers").html(evaluated);
     }
 
     return {
